4593d6cfdd2c9cd4f98f0e093dd1b89f7ec5f6ba
bcthakre <bcthakregcp@gmail.com>
2022-10-11 00:31:05 -0500

    Adding code with error

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { FirestoreAdapter } from '@next-auth/firebase-adapter';
+import { db } from '../../../firebase.config';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: FirestoreAdapter({}),
+
+  // ...
+});
